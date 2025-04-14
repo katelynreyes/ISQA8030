393af49b6ebc878bc9ec67457641587d7c1d06e2
Kaleb Koehn <kaleb.b.koehn@gmail.com>
2025-04-13 20:53:51 -0500

    added admin contact happenings listings and login apps to core project

--- DjangoProject/urls.py
+++ DjangoProject/urls.py
@@ -3,5 +3,10 @@ from django.urls import include, path
 
 urlpatterns = [
     path("home/", include("home.urls")),
+path("administration/", include("admin.urls")),
+path("contact/", include("contact.urls")),
+path("happenings/", include("happenings.urls")),
+path("listings/", include("listings.urls")),
+path("login/", include("login.urls")),
     path("admin/", admin.site.urls),
 ]
