3fdfed9f8604546be1ca145b290e6c1162b09ea0
Kaleb Koehn <kaleb.b.koehn@gmail.com>
2025-04-13 21:03:12 -0500

    added a default empty url pattern to redirect to home

--- DjangoProject/urls.py
+++ DjangoProject/urls.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("", include("home.urls")),
     path("home/", include("home.urls")),
-path("administration/", include("admin.urls")),
-path("contact/", include("contact.urls")),
-path("happenings/", include("happenings.urls")),
-path("listings/", include("listings.urls")),
-path("login/", include("login.urls")),
+    path("administration/", include("admin.urls")),
+    path("contact/", include("contact.urls")),
+    path("happenings/", include("happenings.urls")),
+    path("listings/", include("listings.urls")),
+    path("login/", include("login.urls")),
     path("admin/", admin.site.urls),
 ]
