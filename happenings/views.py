0e4815bf6a408d247ed5648a560c8d5a983f25cd
Kaleb Koehn <kaleb.b.koehn@gmail.com>
2025-04-17 19:01:04 -0500

    Removed all template and pointed to index with formatting

--- happenings/views.py
+++ happenings/views.py
@@ -5,12 +5,9 @@ from happenings.models import Happening
 
 
 def index(request):
-    return HttpResponse("Happenings app")
-
-def all(request):
     all_happenings = Happening.objects.order_by('happening_created_date')
     context = {"all_happenings": all_happenings}
-    return render(request, "happenings/all.html", context)
+    return render(request, "happenings/index.html", context)
 
 def details(request, happening_id):
     happening = get_object_or_404(Happening, pk=happening_id)
