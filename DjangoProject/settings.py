0bccacf84641028a607f46fe42a4a2d0af2bbe38
Kaleb Koehn <kaleb.b.koehn@gmail.com>
2025-04-17 20:00:05 -0500

    Updated settings.py for static directory

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -127,10 +127,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-STATIC_ROOT = '/home/kbkoehn/ISQA8030/static'
+if DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / "static",
+    ]
+else:
+    STATIC_ROOT = '/home/kbkoehn/ISQA8030/static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
