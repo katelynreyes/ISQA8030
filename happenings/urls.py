5b1991cf736cb670a2365526a8b368a96e257e5d
Kaleb Koehn <kaleb.b.koehn@gmail.com>
2025-04-17 19:01:04 -0500

    Removed all template and pointed to index with formatting

--- happenings/urls.py
+++ happenings/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("all", views.all, name="all"),
     path("<int:happening_id>/", views.details, name="details"),
 ]
