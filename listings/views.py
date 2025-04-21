767f72bf8d2fb34ab151bcb9d1a5ffa9c27a7585
Kaleb Koehn <kaleb.b.koehn@gmail.com>
2025-04-20 22:02:52 -0500

    Added basic listing detail information

--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from listings.models import listing
 
 def index(request):
     return HttpResponse("Listings app")
+
+def details(request, listing_id):
+    listingItem = get_object_or_404(listing, pk=listing_id)
+    return render(request, "listings/details.html", {"listingItem": listingItem})
